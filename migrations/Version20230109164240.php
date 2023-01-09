38d53df350a48bdb61f209c0a603a5d5dee865c1
danileau <danilo.licitra@gmail.com>
2023-01-09 17:55:12 +0100

    deployment issue

--- migrations/Version20230109164240.php
+++ migrations/Version20230109164240.php
@@ -20,6 +20,7 @@ final class Version20230109164240 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, birthdate DATE DEFAULT NULL, gender VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE calipometrie (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, bauch INT DEFAULT NULL, brust INT DEFAULT NULL, huefte INT DEFAULT NULL, trizeps INT DEFAULT NULL, bein INT DEFAULT NULL, achsel INT DEFAULT NULL, ruecken INT DEFAULT NULL, timestamp DATETIME NOT NULL, bodyfat DOUBLE PRECISION DEFAULT NULL, INDEX IDX_80816587A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
@@ -31,7 +32,7 @@ final class Version20230109164240 extends AbstractMigration
         $this->addSql('CREATE TABLE pal (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, schlaf INT NOT NULL, liegend INT NOT NULL, sitzend INT NOT NULL, gehend INT NOT NULL, stehend INT NOT NULL, sport INT NOT NULL, timestamp DATETIME NOT NULL, value DOUBLE PRECISION NOT NULL, INDEX IDX_9352C6D79D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, birthdate DATE DEFAULT NULL, gender VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        
     }
 
     public function down(Schema $schema): void
